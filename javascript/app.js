5cd6ed1df8a231d6666cc4cca2f1935f67953bae
Nyra Dev <37726406+akinwol@users.noreply.github.com>
2018-07-07 13:27:02 -0400

    geocoding added

--- javascript/app.js
+++ javascript/app.js
@@ -2,9 +2,20 @@ function initMap(){
     
     function activateSearch(){
         var input = document.getElementById('search-term');
-       var autoComplete = new google.maps.places.Autocomplete(input)
+       var autoComplete = new google.maps.places.Autocomplete(input);
+       google.maps.event.addListener(autoComplete, 'place_changed', function(){
+        var place = autoComplete.getPlace();
+        var name = place.name;
+        var lat = place.geometry.location.lat();
+        var lng = place.geometry.location.lng();
+        console.log("name: " + name + " lat: " + lat + " lng: " + lng)
+       });
       
-    
+       
     };
     activateSearch();
+   
+
+
 }
+
