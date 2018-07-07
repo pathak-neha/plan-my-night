de185e6a88a41ac6658bea245f744f4d7a78ce16
Nyra Dev <37726406+akinwol@users.noreply.github.com>
2018-07-07 12:12:18 -0400

    new js file, updated search on html

---
+++ javascript/app.js
@@ -0,0 +1,10 @@
+function initMap(){
+    
+    function activateSearch(){
+        var input = document.getElementById('search-term');
+       var autoComplete = new google.maps.places.Autocomplete(input)
+      
+    
+    };
+    activateSearch();
+}
