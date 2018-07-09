3c8058d9a4f5bf84a1bff2a902d4fdb6ab7de694
Neha Pathak <neha.pathak@hotmail.com>
2018-07-09 19:57:13 -0400

    initial commit for Eventful database pull

--- javascript/app.js
+++ javascript/app.js
@@ -17,5 +17,31 @@ function initMap(){
    
 
 
-}
+};
+
+$('#search-btn').on('click', function (event) {
+    event.preventDefault();
+    lat = 43.6482130; // temporary - to be linked to Maps geolocation data
+    lng = -79.3890950; // temporary - to be linked to Maps geolocation data
+    var radius = 25;
+    var eventfulKey = 'WF6X75bcVKvW7tKm';
+    var oArgs = {
+        app_key: eventfulKey,
+        where: lat + ',' + lng,
+        within: 25,
+        "date": "2018071900-2018081900", // temporary - to be linked to today's date 
+        page_size: 25,
+        sort_order: "popularity"
+    };
+
+    EVDB.API.call("/events/search", oArgs, function (oData) {
+        var result = oData;
+        var eventsArray = result.events.event;
+        console.log(eventsArray);
+        for (i=0; i < eventsArray.length; i++) {
+            var num = i+1
+            $('#eventful-results').append('Event #'+num+': '+eventsArray[i]['title']+'<br>');
+        };
+    });
+});
 
