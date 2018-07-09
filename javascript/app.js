56c16e4670ec5da8ba1be23929824c3fc0e9393b
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-07-08 22:56:03 -0400

    nearby results function

--- javascript/app.js
+++ javascript/app.js
@@ -9,7 +9,7 @@ function initMap(){
     activateSearch();
 }
 
-function geoFire(resultsArray, nameProperty, latProperty, lngProperty) {
+function geoFire_set(resultsArray, nameProperty, latProperty, lngProperty) {
     //  -- converts results array to an object and saves to a geoFire instance
     //  -- first parameter is a results array
     //  -- other parameters take strings identifying the name, latitude, longitude property
@@ -36,4 +36,22 @@ function geoFire(resultsArray, nameProperty, latProperty, lngProperty) {
         })
 
     return newGeoFireInstance;
+}
+
+function geoFire_getNearby(geoFireObj, loc, rad) {
+    //  -- takes a geoFire object, location [lat, long], rad(km)
+    //  -- returns an array of names (as stored in database index) of stored locations within the search radius
+
+    var results = [];
+    var query = geoFireObj.query({center: loc, radius: rad});
+    query.on('key_entered', function(key, loc, distance) {
+        geoFireObj.ref().once('value', function(snap) {
+                var name = snap.val().index[key].name;
+                results.push(name);
+
+                console.log(name + ' is ' + Math.round(distance) + ' km from this location');
+            })
+    });
+
+    return results;
 }
