b027f48d540d18f12486b5ee5e9df67001e6cc5e
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-07-08 22:50:30 -0400

    geofire_set function

--- javascript/app.js
+++ javascript/app.js
@@ -8,3 +8,32 @@ function initMap(){
     };
     activateSearch();
 }
+
+function geoFire(resultsArray, nameProperty, latProperty, lngProperty) {
+    //  -- converts results array to an object and saves to a geoFire instance
+    //  -- first parameter is a results array
+    //  -- other parameters take strings identifying the name, latitude, longitude property
+    //  for this results set
+    //  -- returns geofire object
+
+    var newRef = firebase.database().ref('Geofire').push();
+    var newGeoFireInstance = new GeoFire(newRef);
+
+    var buildObject = new Object();
+    resultsArray.forEach(function(newLocation, i) {
+        coords = [parseFloat(newLocation[latProperty]), parseFloat(newLocation[lngProperty])];
+        buildObject[i] = coords;
+        
+        firebase.database()
+            .ref('Geofire/' + newRef.key + '/index/' + i)
+            .set({name: newLocation[nameProperty]});
+    })
+
+    newGeoFireInstance
+        .set(buildObject)
+        .then(function() {
+            console.log('New locations have been saved to firebase.');
+        })
+
+    return newGeoFireInstance;
+}
