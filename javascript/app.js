e8d885c92c30faaef8679f8ef76b87dbbc15b9c4
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-07-15 22:24:10 -0400

    savepoint

--- javascript/app.js
+++ javascript/app.js
@@ -444,7 +444,7 @@ $(document).ready(function () {
       if (obj.image !== null) {
         newObj.imageURL = obj.image.medium.url;
       } else {
-        newObj.imageURL = 'https://picsum.photos/128?image=1075';
+        newObj.imageURL = 'https://picsum.photos/128?image=56';
       }
       return newObj;
     })
@@ -467,6 +467,7 @@ $(document).ready(function () {
     getNearbyRestaurants(loc, 1000).then(function (response) {
 
       restaurantResultsArray = response;
+      // geoFire_set(response, 'name', 'location');
 
       // render restaurant results to html
       $('#eventful-results').empty();
@@ -508,7 +509,6 @@ $(document).ready(function () {
 
         $('#eventful-results').append(newDiv);
 
-        
       })
 
     })
@@ -529,8 +529,6 @@ $(document).ready(function () {
         location: response.formatted_address
       }
       locationsToMap.push(locationObj);
-      console.log('locations to map -----------');
-      console.log(locationsToMap);
     })
 
     var latlong = new google.maps.LatLng(restaurantResultsArray[index].location[0], restaurantResultsArray[index].location[1]);
@@ -588,60 +586,31 @@ $(document).ready(function () {
 
 });
 
-function geoFire_set(resultsArray, nameProperty, latProperty, lngProperty) {
-    //  -- converts results array to an object and saves to a geoFire instance
-    //  -- first parameter is a results array
-    //  -- other parameters take strings identifying the name, latitude, longitude property
-    //  for this results set
-    //  -- returns geofire object
+// function geoFire_set(resultsArray, nameProperty, locProperty) {
+//     //  -- converts results array to an object and saves to a geoFire instance
+//     //  -- first parameter is a results array
+//     //  -- other parameters take strings identifying the name, location [lat, lng] property
+//     //  for this results set
+//     //  -- returns geofire object
 
-    var newRef = firebase.database().ref('Geofire').push();
-    var newGeoFireInstance = new GeoFire(newRef);
+//     var newRef = firebase.database().ref('Geofire').push();
+//     var newGeoFireInstance = new GeoFire(newRef);
 
-    var buildObject = new Object();
-    resultsArray.forEach(function(newLocation, i) {
-        coords = [parseFloat(newLocation[latProperty]), parseFloat(newLocation[lngProperty])];
-        buildObject[i] = coords;
+//     var buildObject = new Object();
+//     resultsArray.forEach(function(newLocation, i) {
+//         coords = [parseFloat(newLocation[locProperty][0]), parseFloat(newLocation[locProperty][1])];
+//         buildObject[i] = coords;
         
-        firebase.database()
-            .ref('Geofire/' + newRef.key + '/index/' + i)
-            .set({name: newLocation[nameProperty]});
-    })
+//         firebase.database()
+//             .ref('Geofire/' + newRef.key + '/index/' + i)
+//             .set({name: newLocation[nameProperty]});
+//     })
 
-    newGeoFireInstance
-        .set(buildObject)
-        .then(function() {
-            console.log('New locations have been saved to firebase.');
-        })
+//     newGeoFireInstance
+//         .set(buildObject)
+//         .then(function() {
+//             console.log('New locations have been saved to firebase.');
+//         })
 
-    return newGeoFireInstance;
-}
-
-function geoFire_getNearby(geoFireObj, loc, rad) {
-    //  -- takes a geoFire object, location [lat, long], rad(km)
-    //  -- returns an array of names (as stored in database index) of stored locations within the search radius
-
-    var results = [];
-    var query = geoFireObj.query({center: loc, radius: rad});
-    query.on('key_entered', function(key, loc, distance) {
-        geoFireObj.ref().child('index/' + key).once('value', function(snap) {
-                var name = snap.val().name;
-                results.push(name);
-
-                console.log(name + ' is ' + Math.round(distance) + ' km from this location');
-            })
-    });
-
-    return results;
-}
-
-function findMidpoint(location1, location2) {
-    // -- finds midpoint between locations [lat, lng]
-    // -- returns midpoint [lat, lng]
-
-    var midLat = (location1[0] + location2[0]) / 2;
-    var midLng = (location1[1] + location2[1]) / 2;
-    var midpoint = [midLat, midLng];
-
-    return midpoint;
-}
+//     return newGeoFireInstance;
+// }
